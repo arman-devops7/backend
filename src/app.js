de1b9ca1ddd9922f4bcc12c624f0033d03eb20dd
arman-devops7 <armanmukadamdevops@gmail.com>
2025-01-13 20:46:13 +0530

    completed setup of user routing and controller

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import express, { urlencoded } from "express";
+import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
@@ -13,5 +13,10 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public")); // to store public assets
 app.use(cookieParser()); // to access cookies of user browser in our server
+import userRouter from "./routes/user.routes.js";
+
+// routes
+// routes decalration
+app.use("/api/v1/users", userRouter) //http://localhost:3000/api/v1/users
 
 export { app }
