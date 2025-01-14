0c30b005e692c37aa8f3696bafa223d9d9ec10b4
arman-devops7 <armanmukadamdevops@gmail.com>
2025-01-15 00:39:44 +0530

    fixed syntax

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -135,24 +135,24 @@ const logoutUser = asyncHandler(async (req, res) => {
     {
       $set: {
         refreshToken: undefined
-      },
+      }
+    },
       {
       new: true
     }
-    }
-)
-// cookies clear
-const options = {
-  httpOnly: true,
-  secure: true,
-}
-return res
-  .status(200)
-  .clearCookie("accessToken", options)
-  .clearCookie("refreshToken", options)
-  .json(
-    new ApiResponse(200, {}, "user Logged Out successfully")
   )
+  // cookies clear
+  const options = {
+    httpOnly: true,
+    secure: true,
+  }
+  return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(
+      new ApiResponse(200, {}, "user Logged Out successfully")
+    )
 })
 
 export {
